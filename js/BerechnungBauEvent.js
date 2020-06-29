bceaa9dac8531003d828ee13a9f21573efa4ab5b
sasori9969 <64680421+sasori9969@users.noreply.github.com>
2020-06-29 15:40:13 +0200

    Update BerechnungBauEvent.js

--- js/BerechnungBauEvent.js
+++ js/BerechnungBauEvent.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
         bspholz = document.getElementById('bspholz');
         bsilber = document.getElementById('bsilber');
         var summe = bnahrung.valueAsNumber / 1000 + bholz.valueAsNumber * 3 / 160 + bsilber.valueAsNumber * 5 / 40 + bspnahrung.valueAsNumber * 1500 + bspholz.valueAsNumber * 4500 + bspsilber.valueAsNumber * 7500;
-        summe = summe.toLocaleString();
+        summe = summe.toLocaleString(5);
 
 
         document.getElementById('betrag1').value = summe;
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("myForm1").addEventListener("input", rechne);
 
 });
+
 
 
 summe = summe.toFixed(0);
