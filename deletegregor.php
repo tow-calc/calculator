f8a3f67e8409d9f0204066bd0c0cf33e74495449
sasori9969 <64680421+sasori9969@users.noreply.github.com>
2020-07-22 08:54:49 +0200

    small changes

---
+++ deletegregor.php
@@ -0,0 +1,10 @@
+<?php
+            $con = mysqli_connect('rdbms.strato.de', 'U4173818', 'Iwoiweis18+', 'DB4173818');
+
+            $sql = "DELETE FROM Gregor WHERE ID='$_GET[id]'";
+
+            if(mysqli_query($con,$sql))
+                header("refresh:1; url=gregor.php");
+                else 
+                echo "Nicht gelöscht";
+                ?>
--- gregor.php
+++ gregor.php
@@ -17,68 +17,229 @@
             <a href="#handel">Handelsschiff Event</a>
             <br>
 
-
             <form action="gregor.php" method="POST">
-                <label for="nahrung">Nahrung</label><br />
-                <input name="nahrung" type="text" /> <br />
+                <label for="nahrung">Nahrung</label>
+                <input name="nahrung" type="text" />
+                <label for="nahrung">Holz</label>
+                <input name="nahrung" type="text" />
+                <label for="holz">Silber</label>
+                <input name="holz" type="text" />
                 <br>
-                <label for="holz">Holz</label><br />
-                <input name="holz" type="text" /> <br />
+                <label for="spNahrung">Spezial Nahrung</label>
+                <input name="spNahrung" type="text" />
+                <label for="spHolz">Spezial Holz</label>
+                <input name="spHolz" type="text" />
+                <label for="spSilber">Spezial Silber</label>
+                <input name="spSilber" type="text" />
                 <br>
-                <label for="silber">Silber</label><br />
-                <input name="silber" type="text" /> <br />
                 <br>
-                <h4>test1</h4>
+                <label for="schiff1">Schiff 1</label>
+                <select name="schiff1">
+                    <option value=""> Schiff 1 Auswahl </option>
+                    <option value="Roter Drache"> Roter Drache </option>
+                    <option value="Blaue Sirene"> Blaue Sirene </option>
+                    <option value="Sturmreiter"> Sturmreiter </option>
+                    <option value="Neptuns Ruhm"> Neptuns Ruhm </option>
+                    <option value="Gewaltige Galeone"> Gewaltige Galeone </option>
+                    <option value="Kriegsgaleone"> Kriegsgaleone </option>
+                    <option value="Superfregatte"> Superfregatte </option>
+                </select>
+                <label for="schiff2">Schiff 2</label>
+                <select name="schiff2">
+                    <option value=""> Schiff 2 Auswahl </option>
+                    <option value="Roter Drache"> Roter Drache </option>
+                    <option value="Blaue Sirene"> Blaue Sirene </option>
+                    <option value="Sturmreiter"> Sturmreiter </option>
+                    <option value="Neptuns Ruhm"> Neptuns Ruhm </option>
+                    <option value="Gewaltige Galeone"> Gewaltige Galeone </option>
+                    <option value="Kriegsgaleone"> Kriegsgaleone </option>
+                    <option value="Superfregatte"> Superfregatte </option>
+                </select>
+                <label for="schiff3">Schiff 3</label>
+                <select name="schiff3">
+                    <option value=""> Schiff 3 Auswahl </option>
+                    <option value="Roter Drache"> Roter Drache </option>
+                    <option value="Blaue Sirene"> Blaue Sirene </option>
+                    <option value="Sturmreiter"> Sturmreiter </option>
+                    <option value="Neptuns Ruhm"> Neptuns Ruhm </option>
+                    <option value="Gewaltige Galeone"> Gewaltige Galeone </option>
+                    <option value="Kriegsgaleone"> Kriegsgaleone </option>
+                    <option value="Superfregatte"> Superfregatte </option>
+                </select>
+                <label for="schiff4">Schiff 4</label>
+                <select name="schiff4">
+                    <option value=""> Schiff 4 Auswahl </option>
+                    <option value="Roter Drache"> Roter Drache </option>
+                    <option value="Blaue Sirene"> Blaue Sirene </option>
+                    <option value="Sturmreiter"> Sturmreiter </option>
+                    <option value="Neptuns Ruhm"> Neptuns Ruhm </option>
+                    <option value="Gewaltige Galeone"> Gewaltige Galeone </option>
+                    <option value="Kriegsgaleone"> Kriegsgaleone </option>
+                    <option value="Superfregatte"> Superfregatte </option>
+                </select>
+                <label for="schiff5">Schiff 5</label>
+                <select name="schiff5">
+                    <option value=""> Schiff 5 Auswahl </option>
+                    <option value="Roter Drache"> Roter Drache </option>
+                    <option value="Blaue Sirene"> Blaue Sirene </option>
+                    <option value="Sturmreiter"> Sturmreiter </option>
+                    <option value="Neptuns Ruhm"> Neptuns Ruhm </option>
+                    <option value="Gewaltige Galeone"> Gewaltige Galeone </option>
+                    <option value="Kriegsgaleone"> Kriegsgaleone </option>
+                    <option value="Superfregatte"> Superfregatte </option>
+                </select>
+                <br>
+                <label for="schiff6">Schiff 6</label>
+                <select name="schiff6">
+                    <option value=""> Schiff 6 Auswahl </option>
+                    <option value="Roter Drache"> Roter Drache </option>
+                    <option value="Blaue Sirene"> Blaue Sirene </option>
+                    <option value="Sturmreiter"> Sturmreiter </option>
+                    <option value="Neptuns Ruhm"> Neptuns Ruhm </option>
+                    <option value="Gewaltige Galeone"> Gewaltige Galeone </option>
+                    <option value="Kriegsgaleone"> Kriegsgaleone </option>
+                    <option value="Superfregatte"> Superfregatte </option>
+                </select>
+                <label for="schiff7">Schiff 7</label>
+                <select name="schiff7">
+                    <option value=""> Schiff 7 Auswahl </option>
+                    <option value="Roter Drache"> Roter Drache </option>
+                    <option value="Blaue Sirene"> Blaue Sirene </option>
+                    <option value="Sturmreiter"> Sturmreiter </option>
+                    <option value="Neptuns Ruhm"> Neptuns Ruhm </option>
+                    <option value="Gewaltige Galeone"> Gewaltige Galeone </option>
+                    <option value="Kriegsgaleone"> Kriegsgaleone </option>
+                    <option value="Superfregatte"> Superfregatte </option>
+                </select>
+                <label for="schiff8">Schiff 8</label>
+                <select name="schiff8">
+                    <option value=""> Schiff 8 Auswahl </option>
+                    <option value="Roter Drache"> Roter Drache </option>
+                    <option value="Blaue Sirene"> Blaue Sirene </option>
+                    <option value="Sturmreiter"> Sturmreiter </option>
+                    <option value="Neptuns Ruhm"> Neptuns Ruhm </option>
+                    <option value="Gewaltige Galeone"> Gewaltige Galeone </option>
+                    <option value="Kriegsgaleone"> Kriegsgaleone </option>
+                    <option value="Superfregatte"> Superfregatte </option>
+                </select>
+                <label for="schiff9">Schiff 9</label>
+                <select name="schiff9">
+                    <option value=""> Schiff 9 Auswahl </option>
+                    <option value="Roter Drache"> Roter Drache </option>
+                    <option value="Blaue Sirene"> Blaue Sirene </option>
+                    <option value="Sturmreiter"> Sturmreiter </option>
+                    <option value="Neptuns Ruhm"> Neptuns Ruhm </option>
+                    <option value="Gewaltige Galeone"> Gewaltige Galeone </option>
+                    <option value="Kriegsgaleone"> Kriegsgaleone </option>
+                    <option value="Superfregatte"> Superfregatte </option>
+                </select>
+                <label for="schiff10">Schiff 10</label>
+                <select name="schiff10">
+                    <option value=""> Schiff 10 Auswahl </option>
+                    <option value="Roter Drache"> Roter Drache </option>
+                    <option value="Blaue Sirene"> Blaue Sirene </option>
+                    <option value="Sturmreiter"> Sturmreiter </option>
+                    <option value="Neptuns Ruhm"> Neptuns Ruhm </option>
+                    <option value="Gewaltige Galeone"> Gewaltige Galeone </option>
+                    <option value="Kriegsgaleone"> Kriegsgaleone </option>
+                    <option value="Superfregatte"> Superfregatte </option>
+                </select>
+                <br>
+                <br>
                 <button type="submit" name="speichern" value="speichern">SPEICHERN!</button>
             </form>
-        
+
             <?php
-        
+
             if (isset($_POST['speichern'])) {
-        
+
                 require('Include/db_connect.php');
-        
-        
+
+
                 $nahrung = $_POST['nahrung'];
                 $holz = $_POST['holz'];
                 $silber = $_POST['silber'];
-       
-        
+                $spNahrung = $_POST['spNahrung'];
+                $spHolz = $_POST['spHolz'];
+                $spSilber = $_POST['spSilber'];
+                $schiff1 = $_POST['schiff1'];
+                $schiff2 = $_POST['schiff2'];
+                $schiff3 = $_POST['schiff3'];
+                $schiff4 = $_POST['schiff4'];
+                $schiff5 = $_POST['schiff5'];
+                $schiff6 = $_POST['schiff6'];
+                $schiff7 = $_POST['schiff7'];
+                $schiff8 = $_POST['schiff8'];
+                $schiff9 = $_POST['schiff9'];
+                $schiff10 = $_POST['schiff10'];
+
+
+
+
                 echo "eingetragene Nahrung: " . $nahrung;
                 echo "<br>";
                 echo "eingetragenes Holz: " . $holz;
                 echo "<br>";
-                echo "eingetragenes Silber: " . $silber;       
-        
-                $sql = "INSERT INTO Gregor(Nahrung, Holz, Silber) values (:nahrung, :holz, :silber)";
+                echo "eingetragenes Silber: " . $silber;
+                echo "<br>";
+                echo "eingetragene Spezial Nahrung: " . $spNahrung;
+                echo "<br>";
+                echo "eingetragenes Spezial Holz: " . $spHolz;
+                echo "<br>";
+                echo "eingetragenes Spezial Silber: " . $spSilber;
+
+
+
+
+                $sql = "INSERT INTO Gregor(Nahrung, Holz, Silber, spNahrung, spHolz, spSilber) values (:nahrung, :holz, :silber, :spNahrung, :spHolz, :spSilber)";
                 $stmt = $dbh->prepare($sql);
                 $stmt->bindValue(':nahrung', $nahrung);
                 $stmt->bindValue(':holz', $holz);
                 $stmt->bindValue(':silber', $silber);
-        
+                $stmt->bindValue(':spNahrung', $spNahrung);
+                $stmt->bindValue(':spHolz', $spHolz);
+                $stmt->bindValue(':spSilber', $spSilber);
+
+
                 $stmt->execute();
             }
-        
-            require('connectBD.php');
-        
-            $db_res = runSQL("Select Nahrung, Holz, Silber FROM Gregor");
-        
 
-            echo ('<table>');
+            require('connectBD.php');
+
+            $db_res = runSQL("Select ID, Nahrung, Holz, Silber, spNahrung, spHolz, spSilber  FROM Gregor");
+
+            echo "<table border='1' cellpadding='5'>
+            <tr>
+            <th>ID</th>
+            <th>Nahrung</th>
+            <th>Holz</th>
+            <th>Silber</th>
+            <th>Sp Nahrung</th>
+            <th>Sp Holz</th>
+            <th>Sp Silber</th>
+            <th>löschen</th>
+            </tr>";
+            echo "<form method='post'>";
+
+
+
+
             while ($row = mysqli_fetch_array($db_res)) {
-                echo ('<tr>');
+                echo ('<td>' . $row['ID'] . '</td>');
                 echo ('<td>' . $row['Nahrung'] . '</td>');
                 echo ('<td>' . $row['Holz'] . '</td>');
                 echo ('<td>' . $row['Silber'] . '</td>');
-        
+                echo ('<td>' . $row['spNahrung'] . '</td>');
+                echo ('<td>' . $row['spHolz'] . '</td>');
+                echo ('<td>' . $row['spSilber'] . '</td>');
+                echo "<td> <a href=deletegregor.php?id=" . $row['ID'] . ">Delete</a> </td>";
                 echo ('</tr>');
             }
-            echo ('</table>');
 
-        
-        
+
+
             ?>
-        
+
 
 
 </body>
